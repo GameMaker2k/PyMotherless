333d44176f053c1409e53c35074ceb49658f9c45
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2016-02-28 08:14:18 -0600

    Small update to demo.

--- pymotherless-demo.py
+++ pymotherless-demo.py
@@ -18,6 +18,7 @@
 
 from __future__ import division, absolute_import, print_function;
 import re, os, sys, pymotherless, argparse;
+import logging as log;
 
 __project__ = pymotherless.__project__;
 __program_name__ = pymotherless.__program_name__;
@@ -54,6 +55,7 @@ geturls_download_sleep = pymotherless.geturls_download_sleep;
 
 parser = argparse.ArgumentParser(description="get urls of images/videos from motherless.com", conflict_handler="resolve", add_help=True);
 parser.add_argument('-v', '--version', action='version', version=__program_name__+" "+__version__);
+parser.add_argument("-V", "--verbose", action = "store_true", help = "print various debugging information");
 getargs = parser.parse_args();
 
 if(getargs.verbose==True):
